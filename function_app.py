4063397fe4a89a9aea79ff28fe76cb0b2e8bb36b
hvalfangst <hvalfangst@tidehunter.eu>
2024-01-04 20:38:02 +0100

    Associated Flask endpoints with their own dedicated Azure Function

--- function_app.py
+++ function_app.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify, request
+import logging
 
-from flask_api.crypto_utils.module import aes_encrypt, aes_decrypt
+import azure.functions as func
+from flask import Flask, jsonify, request
+from crypto_utils import aes_encrypt, aes_decrypt
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @app.route("/")
 def index():
+    logger.info("Received request to index endpoint.")
     return (
         "Welcome to Hvalfangst Crypto API!\n\n"
         "Available Endpoints:\n"
@@ -19,12 +26,16 @@ def index():
 
 @app.route("/encrypt", methods=["POST"])
 def encrypt():
+    logger.info("Received request to encrypt endpoint.")
+
     # Extract data from JSON body
     request_data = request.get_json()
 
     # Check if 'plain_text' and 'encryption_key' fields are present in the JSON
     if "plain_text" not in request_data or "encryption_key" not in request_data:
-        return jsonify({"error": "Missing 'plain_text' or 'encryption_key' in the request body"}), 400
+        error_msg = "Missing 'plain_text' or 'encryption_key' in the request body"
+        logger.error(error_msg)
+        return jsonify({"error": error_msg}), 400
 
     plain_text = request_data["plain_text"]
     encryption_key = request_data["encryption_key"]
@@ -39,18 +50,24 @@ def encrypt():
         "encrypted_text": encrypted_text
     }
 
+    logger.info("Encryption successful.")
+
     # Return JSON response
     return jsonify(response)
 
 
 @app.route("/decrypt", methods=["POST"])
 def decrypt():
+    logger.info("Received request to decrypt endpoint.")
+
     # Extract data from JSON body
     request_data = request.get_json()
 
     # Check if 'encrypted_text' and 'encryption_key' fields are present in the JSON
     if "encrypted_text" not in request_data or "encryption_key" not in request_data:
-        return jsonify({"error": "Missing 'encrypted_text' or 'encryption_key' in the request body"}), 400
+        error_msg = "Missing 'encrypted_text' or 'encryption_key' in the request body"
+        logger.error(error_msg)
+        return jsonify({"error": error_msg}), 400
 
     encrypted_text = request_data["encrypted_text"]
     encryption_key = request_data["encryption_key"]
@@ -65,9 +82,11 @@ def decrypt():
         "plain_text": plain_text
     }
 
+    logger.info("Decryption successful.")
+
     # Return JSON response
     return jsonify(response)
 
 
-if __name__ == "__main__":
-    app.run()
+# Create an Azure Function which serves the above routes in our WSGI runtime (Gunicorn)
+app = func.WsgiFunctionApp(app=app.wsgi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
--- wsgi_middleware/__init__.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-from flask_api import app
-
-
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    """Each request is redirected to the WSGI handler, which serves our Flask API.
-    """
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
